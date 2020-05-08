c5f167ae761870772ade04674c1f6d5ea8d8003b
Jonathan Mang <jonathan.mang@uk-erlangen.de>
2020-05-08 08:36:56 +0200

    Updates

---
+++ Shell.sh
@@ -0,0 +1,9 @@
+## Mark all
+"1. Cursor to the beginning of file
+2. Ctr + 6 to set a mark
+3. Alt + Shift + T (or try Alt + T) to cut to the end of the file
+4. If Alt + T doesn't work, try Ctrl + K
+5. To just copy the file content do Ctrl + U to uncut the text again"
+
+#  Docker path fix windows:
+export MSYS_NO_PATHCONV=1
